0c77000470e9157ddcef9f8ebe633f97ba85dcd7
GuillaumeBuj <guillaume.bujon@gmail.com>
2022-08-02 12:08:13 +0200

    Authentification et formulaire. A completer(type et contraintes)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,10 +4,12 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['pseudo'], message: 'There is already an account with this pseudo')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -38,6 +40,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $image = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $eMail = null;
 
     public function getId(): ?int
     {
@@ -172,6 +177,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setImage(?string $image): self
     {
         $this->image = $image;
+
+        return $this;
+    }
+
+    public function getEMail(): ?string
+    {
+        return $this->eMail;
+    }
+
+    public function setEMail(string $eMail): self
+    {
+        $this->eMail = $eMail;
 
         return $this;
     }
